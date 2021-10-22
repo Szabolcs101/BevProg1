00b226a6154866b6bd1972f91527d039fa7bb74b
Lakatos Szabolcs <szabolcs1035@gmail.com>
2021-10-22 19:14:15 +0200

    Upload drill5

---
+++ source/drillek/drill5.cpp
@@ -0,0 +1,71 @@
+#include "std_lib_facilities.h" 
+
+int main() 
+try { 
+	
+	//cout << "Success!\n";
+
+	//cout << "Success!\n";
+
+	//cout << "Success" << "!\n";
+
+	//cout << "success!" << '\n';
+
+	//int res = 7; vector<int>v(10); v[5] = res; cout << "Success!\n";
+
+	//vector<int> v(10); v[5] = 7; if (v[5]=7) cout << "Success!\n";
+
+	//if (cout) cout << "Success!\n"; else cout << "Fail!\n";
+
+	//bool c = true; if (c) cout << "Success!\n"; else cout << "Fail!\n";
+
+	//string s = "ape"; bool c = "fool"; vector<int>v(5); for (int i=0; 0; v[5]); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+
+	//string s = "ape"; if (s=="ape") cout << "Success!\n";
+
+	//string s = "ape"; if (s=="ape") cout << "Success!\n";
+
+	//string s = "ape"; if (s=="ape") cout << "Success!\n";
+
+	//vector<int> v(5); for (int i=0; 0; v[5]); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+
+	//vector v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+
+	//string s = "Success\n"; for (int i=0; i<6; ++i) cout << s;
+
+	//if (true) cout << "Success!\n"; else cout << "Fail!\n";
+
+	//int x = 2000; int c = x; if (c==2000) cout << "Success!\n";
+
+	//string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
+
+	//vector<int>v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+
+	//int i=0; int j = 9; while (i<10) ++i; if (j<i) cout << "Success!\n";
+
+	//int x = 2; double d = 5/(x-2); if (d!=2*x+0.5) cout << "Success!\n";
+
+	//string s = "Success!\n"; for (int i=0; i<=10; ++i) cout << s[i];
+
+	//int i=0; int j=9; while (i<10) ++i; if (j<i) cout << "Success!\n";
+
+	//double x = 2; double d = 5/(x-2); if (d=1*x+0.5) cout << "Success!\n";
+	
+	//cout << "Success!\n";
+
+
+
+	keep_window_open(); 
+	return 0;
+} 
+catch (exception& e){  
+	cerr << "error:  " <<e.what() <<'\n'; 
+
+	keep_window_open(); 
+	return 1;
+}   
+catch (...) { 
+cerr <<  "Oops:  unknown exception !\n"; 
+keep_window_open(); 
+return 2; 
+}
